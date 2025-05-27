1542a2c6ad814bfac8fe93b76f004f5e585aad17
Sol8888 <daniela.cabrera.tapia@udla.edu.ec>
2025-05-27 10:25:38 -0500

    Creacion de logs confirmando transacciones

--- Services/transaction_manager.py
+++ Services/transaction_manager.py
@@ -1,6 +1,9 @@
 import json
 import os
 from Models.transaction import Transaction
+from RabbitMQ.direct_producer import send_transaction_confirmation
+from RabbitMQ.fanout_producer import broadcast_notification
+from RabbitMQ.topic_producer import send_account_alert
 
 TRANSACTION_FILE = "Data/transactions.json"
 
@@ -40,7 +43,33 @@ class TransactionManager:
         self.save_transactions()
 
         print(f"Transferencia #{transaction_id} creada.")
+        message = f"Transferencia #{transaction_id}: ${amount:.2f} de {from_account} para {to_account}"
+
+        # Buscar usuarios involucrados
+        to_user = next((u for u in users if u.account_number == to_account), None)
+        from_user = next((u for u in users if u.account_number == from_account), None)
+
+        if to_user:
+            # Confirmación al receptor (Direct)
+            send_transaction_confirmation(to_user.username, message)
+
+            # Alerta por tipo y banco (Topic)
+            routing_key = f"{to_user.account_type}.{to_user.bank_code}"
+            send_account_alert(routing_key, message)
+
+        if from_user:
+            # Confirmación al emisor (Direct)
+            send_transaction_confirmation(from_user.username, message)
+
+            # También podría enviarle alerta por topic si deseas
+            routing_key = f"{from_user.account_type}.{from_user.bank_code}"
+            send_account_alert(routing_key, f"Has enviado: {message}")
+
+        # Fanout para todos
+        broadcast_notification(message)
+
         return transaction
+
 
     def get_transactions_by_account(self, account_number):
         return [t for t in self.transactions if t.from_account == account_number or t.to_account == account_number]
