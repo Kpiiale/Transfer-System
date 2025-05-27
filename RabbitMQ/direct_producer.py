2d0246cc163de3446b816e6967733e5fa6258577
Sol8888 <daniela.cabrera.tapia@udla.edu.ec>
2025-05-27 08:29:15 -0500

    -

--- RabbitMQ/direct_producer.py
+++ RabbitMQ/direct_producer.py
@@ -1,5 +1,5 @@
 import pika
-from config import settings
+from Config import settings
 
 def send_transaction_confirmation(username, message):
     credentials = pika.PlainCredentials(settings.RABBITMQ_USER, settings.RABBITMQ_PASSWORD)
