4d27e7a8eb6ff26dea9dcddd4acc70abc377865d
Sol8888 <yuliana.capito@udla.edu.ec>
2025-05-27 08:19:27 -0500

    Inicio Creacion Productores

---
+++ RabbitMQ/direct_producer.py
@@ -0,0 +1,21 @@
+import pika
+from Config import settings
+
+def send_transaction_confirmation(username, message):
+    credentials = pika.PlainCredentials(settings.RABBITMQ_USER, settings.RABBITMQ_PASSWORD)
+    connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host=settings.RABBITMQ_HOST,
+        port=settings.RABBITMQ_PORT,
+        credentials=credentials
+    ))
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange=settings.EXCHANGE_DIRECT, exchange_type='direct')
+    channel.basic_publish(
+        exchange=settings.EXCHANGE_DIRECT,
+        routing_key=username,
+        body=message.encode()
+    )
+
+    print(f"[Direct] Confirmation sent to {username}: {message}")
+    connection.close()
